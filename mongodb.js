7da7f9f4da70d421bbcb4b432f48e42ea92c12b3
lutfiqaraman <lutfiqaraman@gmail.com>
2020-01-04 23:38:11 +0200

    Add Update and Delete Operations

--- mongodb.js
+++ mongodb.js
@@ -19,19 +19,22 @@ MongoClient.connect(
     // --------------------- Users Operations --------------------
 
     // Insert a new document - Users Database
-    db.collection('users').insertOne({
-      name: 'Hassan Zaid',
-      age: 30
-    }, (error, result) => {
-      if (error) {
-        return process.stdout.write("Unable to connect to Database");
-      }
+    db.collection("users").insertOne(
+      {
+        name: "Hassan Zaid",
+        age: 30
+      },
+      (error, result) => {
+        if (error) {
+          return process.stdout.write("Unable to connect to Database");
+        }
 
-      result.ops;
-    });
+        result.ops;
+      }
+    );
 
     // Find the first record that matches the condition - Users Database
-    db.collection("users").findOne({name: "Lutfi Qaraman"}, (error, user) => {
+    db.collection("users").findOne({ name: "Lutfi Qaraman" }, (error, user) => {
       if (error) {
         return process.stdout.write("Unable to fetch such a user");
       }
@@ -40,20 +43,57 @@ MongoClient.connect(
     });
 
     // Find all but skip the first two records - Users Database
-    db.collection("users").find({name: "Lutfi Qaraman"}).skip(2).toArray((error, users) => {
-      if (error) {
-        return process.stdout.write("Unable to find users");
-      }
-      console.log(users);
-    });
+    db.collection("users")
+      .find({ name: "Lutfi Qaraman" })
+      .skip(2)
+      .toArray((error, users) => {
+        if (error) {
+          return process.stdout.write("Unable to find users");
+        }
+        console.log(users);
+      });
 
     // Show the count of records that matches the condition - Users Database
-    db.collection("users").find({name: "Lutfi Qaraman"}).count((error, count) => {
-      if (error) {
-        return process.stdout.write("Unable to find users");
-      }
-      console.log(count);
-    });
+    db.collection("users")
+      .find({ name: "Lutfi Qaraman" })
+      .count((error, count) => {
+        if (error) {
+          return process.stdout.write("Unable to find users");
+        }
+        console.log(count);
+      });
+
+    // Update the user info - Users Database
+    db.collection("users")
+      .updateOne(
+        {
+          _id: new ObjectID("5e109db90d8bd75770779f77")
+        },
+        {
+          $set: {
+            name: "Mohammad Adnan",
+            age: 25
+          }
+        }
+      )
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    // Delete many users - Users Database
+    db.collection("users")
+      .deleteMany({
+        age: 34
+      })
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        if (error) throw error;
+      });
 
     // --------------------- Tasks Operations --------------------
 
@@ -83,21 +123,25 @@ MongoClient.connect(
     );
 
     // Find the last task record - Tasks Database
-    db.collection("tasks").findOne({ _id: new ObjectID("5e10adf9abf95c1f28250ccd") }, (error, task) => {
-      if (error) {
-        return process.stdout.write("Unable to fetch such a task");
+    db.collection("tasks").findOne(
+      { _id: new ObjectID("5e10adf9abf95c1f28250ccd") },
+      (error, task) => {
+        if (error) {
+          return process.stdout.write("Unable to fetch such a task");
+        }
+
+        console.log(task);
       }
+    );
 
-      console.log(task);
-    });
-
-    // // Find all tasks that are not completed - Tasks Database
-    // db.collection("tasks").find({completed: false}).toArray((error, tasks) => {
-    //   if (error) {
-    //     return process.stdout.write("Unable to fetch tasks");
-    //   }
-    //   console.log(tasks);
-    // });
-
+    // Find all tasks that are not completed - Tasks Database
+    db.collection("tasks")
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        if (error) {
+          return process.stdout.write("Unable to fetch tasks");
+        }
+        console.log(tasks);
+      });
   }
 );
