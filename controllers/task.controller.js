dee92cfa3c208513420229af2a3e849231eaf3f2
lutfiqaraman <lutfiqaraman@gmail.com>
2020-01-11 21:23:37 +0200

    Integrating Async/Await method

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,39 +1,36 @@
 const Task = require("../models/task.model");
 
-exports.create = (req, res) => {
+// Create a new task
+exports.create = async (req, res) => {
     const task = new Task(req.body);
-  
-    task
-      .save()
-      .then(() => {
-          res.status(201).send(task);
-      })
-      .catch((error) => {
-          res.status(400).send(error);
-      });
+
+    try {
+      await task.save();
+      res.status(201).send(task);
+    } catch (error) {
+      res.status(400).send(error);
+    }
   };
 
 // Fetch all tasks
-exports.fetchAll = (req, res) => {
-    Task.find()
-      .then(task => {
-        res.send(task);
-      })
-      .catch(error => {
-        res.status(500).send(error);
-      });
-  };
-  
-  // Fetch Task by ID
-  exports.fetchById = (req, res) => {
-    const _id = req.params.id;
-  
-    Task.findById(_id)
-      .then(task => {
-        res.send(task);
-      })
-      .catch(error => {
-        res.status(500).send(error);
-      });
-  };
+exports.fetchAll = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send(error);
+  }  
+};
+    
+// Fetch Task by ID  
+exports.fetchById = async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }  
+};
   
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,39 +1,36 @@
 const User = require("../models/user.model");
 
-// Create a user
-exports.create = (req, res) => {
+// Create a new user
+exports.create = async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch(error => {
-      res.status(400).send(error);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch(error) {
+    res.status(400).send(error);
+  }
+
 };
 
 // Fetch all users
-exports.fetchAll = (req, res) => {
-  User.find()
-    .then(user => {
-      res.send(user);
-    })
-    .catch(error => {
-      res.status(500).send(error);
-    });
+exports.fetchAll = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 // Fetch User by ID
-exports.fetchById = (req, res) => {
+exports.fetchById = async (req, res) => {
   const _id = req.params.id;
 
-  User.findById(_id)
-    .then(user => {
-      res.send(user);
-    })
-    .catch(error => {
-			res.status(500).send(error);
-    });
+  try {
+    const user = await User.findById(_id);
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
