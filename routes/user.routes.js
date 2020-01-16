3797990ee3a1197533d8d43adb6d2c05b6bb8c43
lutfiqaraman <lutfiqaraman@gmail.com>
2020-01-16 23:48:59 +0200

    Authentication token

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
+const auth = require("../src/middleware/auth");
+
 module.exports = app => {
   const User = require("../controllers/user.controller");
 
   app.post("/users", User.create);
   app.post("/users/login", User.userLogin);
   
-  app.get("/users", User.fetchAll);
+  app.get("/users", auth, User.fetchAll);
   app.get("/users/:id", User.fetchUserById);
   
   app.patch("/users/:id", User.updateUserById);
---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../../models/user.model");
+require("dotenv").config();
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_PRIVATEKEY);
+    const user = await User.findOne({ _id: decoded._id, "tokens.token": token });
+
+    if (!user) {
+      throw new Error;
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "User is not authenticate" });
+  }
+};
+
+module.exports = auth;
