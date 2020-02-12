b7368d6aecd1e08201827a835ed6fec43c3ae3f3
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-12 23:12:25 +0200

    Test connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const server = "127.0.0.1:27017";
-const database = "task-manager-api";
+const databaseName = "task-manager-api";
 
-mongoose.connect(`mongodb://${server}/${database}`, {
+mongoose.connect(`mongodb://${server}/${databaseName}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
--- unit_test/connection.test.js
+++ unit_test/connection.test.js
@@ -1,7 +1,13 @@
-test("Test", () => {
+const mongoose = require("mongoose");
+const server = "127.0.0.1:27017";
+const databaseName = "task-manager-api";
 
-});
-
-test("FailTest", () => {
-    throw new Error("Fail !");
+test("connection", () => {
+    beforeAll(async () => {
+        await mongoose.connect(`mongodb://${server}/${databaseName}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+          }); 
+    });  
 });
