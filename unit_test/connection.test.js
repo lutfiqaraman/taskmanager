5655f1140542cd730ed8328059c7eb5fea9d5f25
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-13 22:49:06 +0200

    Testing User API

--- unit_test/connection.test.js
+++ unit_test/connection.test.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const server = "127.0.0.1:27017";
-const databaseName = "task-manager-api";
+const databaseName = "task-manager-api-test";
 
 test("connection", () => {
     beforeAll(async () => {
