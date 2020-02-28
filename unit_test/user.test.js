ffe25833f9f959396f226c7d96a630715aac92e2
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-29 00:56:26 +0200

    Test the process of creating a user

--- unit_test/user.test.js
+++ unit_test/user.test.js
@@ -1,10 +1,26 @@
 const request = require("supertest");
-const app = require("../src/app");
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "./config/.env.test" });
+
+app.use(express.json());
+require("../routes/user.routes")(app);
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_TEST_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true  
+    });
+});  
 
 test('should signup a new user', async () => {
    await request(app).post("/users/user/create").send({
        name: 'Mr. Test User',
-       email: 'test@email.com',
-       password: 'Password000!'
+       email: 'test@user.com',
+       password: 'Hello@000!'
     }).expect(201);
 });
